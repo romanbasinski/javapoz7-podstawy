84526f99254ffed62c4b525f9c2a6bafbed357ae
Roman Basinski <romanbasinski92@gmail.com>
2018-02-21 20:16:12 +0100

    Nowy

---
+++ src/drugie/Loops.java
@@ -0,0 +1,37 @@
+package drugie;
+
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Insert number of elements to multiply");
+        int n=scanner.nextInt();
+
+
+        for (int i=0;i<n; i=i+1) {
+            int number = scanner.nextInt();
+            if (number >= 5 && number <= 20) {
+
+                System.out.println(i + 1 + ".Pow(" + number + ")=" + (number * number));
+            } else {
+                System.out.println("Upgrade to premium");
+            }
+
+        }
+        System.out.println("End of program");
+      //  int a = scanner.nextInt();
+      //  System.out.println("insert first");
+      //  int b =scanner.nextInt();
+      //  System.out.println("insert second");
+      //  int c=scanner.nextInt();
+      //  System.out.println("insert third");
+       // int d=scanner.nextInt();
+       // System.out.println("insert fourth");
+
+
+        //System.out.println("sum="+(a+b+c));
+
+    }
+}
